78fd2419f8c18eb8602cc42123fc61b22b1e1b50
심승용 <seungyongshim@nexon.co.kr>
2022-05-09 13:39:22 +0900

    .

---
+++ src/main/java/Minefield.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Minefield {
+
+    private List<Cell> cells;
+
+    public Minefield(int width, int height) {
+
+    }
+
+    public List<Cell> getCells() {
+        return cells;
+    }
+    
+}
---
+++ src/test/java/MinefieldTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MinefieldTest {
+    @Test
+    void should_create()
+    {
+        var sut = new Minefield(3, 4);
+
+        assertEquals(12, sut.getCells().size());
+    }
+}
